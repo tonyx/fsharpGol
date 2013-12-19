52e879a7ce325ac884c8d19472fe82355d832812
Antonio Lucca <tonyx@yahoo.com>
2013-12-19 01:28:10 +0000

    accumulator unneeded for filter now

--- GameOfLife.fs
+++ GameOfLife.fs
@@ -10,11 +10,11 @@ module GameOfLife =
         let areNeighbors (x1,y1,_) (x2,y2,_) =
             abs (x1 - x2) <=1 && abs (y1 - y2) <= 1 && not (samePosition (x1,y1) (x2,y2)) 
                                
-        let rec neighbors (acell, grid, accumul) =   
+        let neighbors (acell, grid) =   
             List.filter(fun c -> areNeighbors acell c) grid                     
 
         let neighborsAlive (x,y,s) grid  =
-            List.filter (fun (_,_,s) -> s = Alive) (neighbors( (x,y,s),grid,[]))
+            List.filter (fun (_,_,s) -> s = Alive) (neighbors( (x,y,s),grid))
 
         List.length(neighborsAlive (x,y,s) grid)
  
